56b25c68d2efd19faf6ea4f353a9351c2c668fac
sahidlatif <sahid.latif.ali@gmail.com>
2025-07-04 15:31:29 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Abuot from "./component/abuot";
+import Navbar from "./component/navbar";
+// import TextForm from "./component/textForm";
+function App(){
+return(
+  <>
+
+<Navbar title='TextUtils ' />
+
+<Abuot/>
+{/* <div className="container">
+<TextForm heading='Enter the Text' /> 
+</div> */}
+
+  </>
+)
+}
+export default App;
